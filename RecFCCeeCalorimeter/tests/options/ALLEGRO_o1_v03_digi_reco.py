7c8a2a59ac9876301d2165415a86284a8e4307ae
sss <sss@karma>
2025-01-20 22:25:22 -0500

    Download xtalk file outside of the test.

--- RecFCCeeCalorimeter/tests/options/ALLEGRO_o1_v03_digi_reco.py
+++ RecFCCeeCalorimeter/tests/options/ALLEGRO_o1_v03_digi_reco.py
@@ -401,7 +401,7 @@ if addCrosstalk:
     # read the crosstalk map
     readCrosstalkMap = ReadCaloCrosstalkMap(
         "ReadCrosstalkMap",
-        fileName="https://fccsw.web.cern.ch/fccsw/filesForSimDigiReco/ALLEGRO/ALLEGRO_o1_v03/xtalk_neighbours_map_ecalB_thetamodulemerged.root",
+        fileName="xtalk_neighbours_map_ecalB_thetamodulemerged.root",
         OutputLevel=INFO,
     )
 else:
