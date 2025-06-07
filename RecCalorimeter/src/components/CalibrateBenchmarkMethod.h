6ccf9609005272647b0d0ad39cf34fda1d168e9b
sss <sss@karma>
2025-06-07 10:35:37 -0400

    DataHandle is moving to the k4FWCore namespace.

--- RecCalorimeter/src/components/CalibrateBenchmarkMethod.h
+++ RecCalorimeter/src/components/CalibrateBenchmarkMethod.h
@@ -70,10 +70,10 @@ private:
                        const std::vector<int>& fixedParameters) const;
 
   /// Handle for electromagnetic barrel cells (input collection)
-  mutable DataHandle<edm4hep::CalorimeterHitCollection> m_ecalBarrelCells{"ecalBarrelCells", Gaudi::DataHandle::Reader,
+  mutable k4FWCore::DataHandle<edm4hep::CalorimeterHitCollection> m_ecalBarrelCells{"ecalBarrelCells", Gaudi::DataHandle::Reader,
                                                                           this};
   /// Handle for hadronic barrel cells (input collection)
-  mutable DataHandle<edm4hep::CalorimeterHitCollection> m_hcalBarrelCells{"hcalBarrelCells", Gaudi::DataHandle::Reader,
+  mutable k4FWCore::DataHandle<edm4hep::CalorimeterHitCollection> m_hcalBarrelCells{"hcalBarrelCells", Gaudi::DataHandle::Reader,
                                                                           this};
 
   /// Histogram of total deposited energy in the calorimeters
