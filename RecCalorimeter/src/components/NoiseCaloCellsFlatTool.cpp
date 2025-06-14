760f11685125d08afd5761442b769913903ebe38
sss <sss@karma>
2025-06-13 22:17:38 -0400

    Make noise reproducible.

--- RecCalorimeter/src/components/NoiseCaloCellsFlatTool.cpp
+++ RecCalorimeter/src/components/NoiseCaloCellsFlatTool.cpp
@@ -16,20 +16,48 @@ StatusCode NoiseCaloCellsFlatTool::initialize() {
   return StatusCode::SUCCESS;
 }
 
-template <typename C>
-void NoiseCaloCellsFlatTool::addRandomCellNoiseT (C& aCells) const
+void NoiseCaloCellsFlatTool::initEvent(CLHEP::Ranlux64Engine& e) const
 {
+  const edm4hep::EventHeaderCollection* ehs = m_header.get();
+  edm4hep::EventHeader eh = ehs->at(0);
+  // FIXME: Use all bits of event number.
+  long seeds[] = {static_cast<long>(eh.getRunNumber()),
+                  static_cast<long>(eh.getEventNumber()),
+                  0x76439862,
+                  0};
+  if (seeds[0] == 0) seeds[0] = 0x7fffffff;
+  if (seeds[1] == 0) seeds[1] = 0x7fffffff;
+  e.setSeeds (seeds);
+}
+
+template <typename C>
+void NoiseCaloCellsFlatTool::addRandomCellNoiseT (C& aCells, CLHEP::RandGauss& r) const
+{
+  std::map<uint64_t, typename C::iterator> m;
+  for (auto i = aCells.begin(); i != aCells.end(); ++i) m[i->first] = i;
+  for (auto& p : m) {
+    p.second->second += m_cellNoiseOffset;
+    p.second->second += r.fire() * m_cellNoiseRMS;
+  }
+#if 0
   for (auto& p : aCells) {
     p.second += m_cellNoiseOffset + (m_gauss.shoot() * m_cellNoiseRMS);
   }
+#endif
 }
 
 void NoiseCaloCellsFlatTool::addRandomCellNoise(std::unordered_map<uint64_t, double>& aCells) const {
-  addRandomCellNoiseT (aCells);
+  CLHEP::Ranlux64Engine e;
+  initEvent(e);
+  CLHEP::RandGauss r(e);
+  addRandomCellNoiseT(aCells, r);
 }
 
 void NoiseCaloCellsFlatTool::addRandomCellNoise(std::vector<std::pair<uint64_t, double> >& aCells) const {
-  addRandomCellNoiseT (aCells);
+  CLHEP::Ranlux64Engine e;
+  initEvent(e);
+  CLHEP::RandGauss r(e);
+  addRandomCellNoiseT(aCells, r);
 }
 
 template <typename C>
