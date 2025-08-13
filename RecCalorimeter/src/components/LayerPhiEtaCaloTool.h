3ba7cb7bb808eb2d5fbf6ca04bb36ccf88626fa1
sss <sss@karma>
2025-08-13 11:35:50 -0400

    Make const:   - ICalorimeterTool::prepareEmptyCells   - IGeoSvc::getDetector   - IGeoSvc::constantAsString

--- RecCalorimeter/src/components/LayerPhiEtaCaloTool.h
+++ RecCalorimeter/src/components/LayerPhiEtaCaloTool.h
@@ -23,8 +23,8 @@ class LayerPhiEtaCaloTool : public AlgTool, virtual public ICalorimeterTool {
 public:
   LayerPhiEtaCaloTool(const std::string& type, const std::string& name, const IInterface* parent);
   virtual ~LayerPhiEtaCaloTool() = default;
-  virtual StatusCode initialize() final;
-  virtual StatusCode finalize() final;
+  virtual StatusCode initialize() override final;
+  virtual StatusCode finalize() override final;
   /** Prepare a map of all existing cells in current geometry.
    *   Active layers (cylindrical tubes) are looked in the geometry manager by name ('\b activeVolumeName').
    *   Corresponding bitfield name is given in '\b activeFieldName'.
@@ -37,7 +37,7 @@ public:
    *   @param[out] aCells map of existing cells (and deposited energy, set to 0)
    *   return Status code.
    */
-  virtual StatusCode prepareEmptyCells(std::unordered_map<uint64_t, double>& aCells) final;
+  virtual StatusCode prepareEmptyCells(std::unordered_map<uint64_t, double>& aCells) const override final;
 
 private:
   /// Pointer to the geometry service
