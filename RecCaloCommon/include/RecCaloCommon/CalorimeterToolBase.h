4aa7fb29cb1ac3fdf606c8846b73d839df0023b6
sss <sss@karma>
2025-08-13 11:35:50 -0400

    Remove non-const methods of ICalorimeterTool
    Now that the ICalorimeterTool interface class
    has been updated, we can remove the non-const methods from its
    derived classes (and add override keywords).

--- RecCaloCommon/include/RecCaloCommon/CalorimeterToolBase.h
+++ RecCaloCommon/include/RecCaloCommon/CalorimeterToolBase.h
@@ -32,26 +32,24 @@ public:
 
   /** Standard Gaudi initialize method.
    */
-  virtual StatusCode initialize();
+  virtual StatusCode initialize() override;
 
   /** Return a vector of all existing cells in the current geometry.
    *
    * The result is sorted and unique.
    * Returns an empty vector on error.
    */
-  virtual const std::vector<uint64_t>& cellIDs() const final;
+  virtual const std::vector<uint64_t>& cellIDs() const final override;
 
   /** Prepare a map of all existing cells in current geometry.
    *   @param[out] aCells map of existing cells (and deposited energy, set to 0)
    *   return Status code.
    */
-  virtual StatusCode prepareEmptyCells(std::unordered_map<uint64_t, double>& aCells) const final;
-  virtual StatusCode prepareEmptyCells(std::unordered_map<uint64_t, double>& aCells) final
-  { const auto* cthis = this;  return cthis->prepareEmptyCells(aCells); }
+  virtual StatusCode prepareEmptyCells(std::unordered_map<uint64_t, double>& aCells) const final override;
 
   /** Return the segmentation associated with this geometry.
    */
-  virtual const dd4hep::DDSegmentation::Segmentation* segmentation() const final;
+  virtual const dd4hep::DDSegmentation::Segmentation* segmentation() const final override;
 
   /** Return the name specified for the readout.
    */
