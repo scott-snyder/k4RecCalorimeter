74f7aa9d4a2ae142d8a9bf496ee5d23871909e1b
sss <sss@karma>
2025-08-14 13:40:38 -0400

    Refresh of cellIDs

--- RecFCCeeCalorimeter/src/components/TubeLayerModuleThetaCaloTool.h
+++ RecFCCeeCalorimeter/src/components/TubeLayerModuleThetaCaloTool.h
@@ -1,5 +1,5 @@
-#ifndef RECFCCEECALORIMETER_TUBELAYERMODULETHETAMERGEDCALOTOOL_H
-#define RECFCCEECALORIMETER_TUBELAYERMODULETHETAMERGEDCALOTOOL_H
+#ifndef RECFCCEECALORIMETER_TUBELAYERMODULETHETACALOTOOL_H
+#define RECFCCEECALORIMETER_TUBELAYERMODULETHETACALOTOOL_H
 
 #include "RecCaloCommon/CalorimeterToolBase.h"
 
@@ -31,18 +31,6 @@ public:
 
   virtual StatusCode initialize() override final;
 
-  /** Prepare a map of all existing cells in current geometry.
-   *   Active layers (cylindrical tubes) are looked in the geometry manager by name ('\b activeVolumeName').
-   *   Corresponding bitfield name is given in '\b activeFieldName'.
-   *   If users wants to limit the number of active layers, it is possible by setting '\b activeVolumesNumber'.
-   *   The total number of cells N = n_layer * n_theta * n_phi, where
-   *   n_layer is number of layers (taken from geometry if activeVolumesNumber not set),
-   *   n_theta is number of eta bins in that layer,
-   *   n_phi is number of phi bins (the same for each layer).
-   *   For more explanation please [see reconstruction documentation](@ref md_reconstruction_doc_reccalorimeter).
-   *   @param[out] aCells map of existing cells (and deposited energy, set to 0)
-   *   return Status code.
-   */
 
 protected:
   virtual StatusCode collectCells(std::function<void(uint64_t)> cellFunc) const override final;
@@ -62,4 +50,4 @@ private:
   Gaudi::Property<unsigned int> m_activeVolumesNumber{this, "activeVolumesNumber", 0};
 };
 
-#endif /* RECFCCEECALORIMETER_TUBELAYERMODULETHETAMERGEDCALOTOOL_H */
+#endif /* RECFCCEECALORIMETER_TUBELAYERMODULETHETACALOTOOL_H */
