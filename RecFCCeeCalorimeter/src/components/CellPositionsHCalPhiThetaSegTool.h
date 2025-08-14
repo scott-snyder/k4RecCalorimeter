5fe830bc09ab0ce0047e9f12ee4b63aaa55d5c7f
sss <sss@karma>
2025-08-14 13:59:54 -0400

    Adjust HCal positioning tool for updates in HCal segmentation.
    Update CellPositionsHCalPhiThetaSegTool to work properly when the HCal
    theta segmentation is changed to properly return the position in the volume
    coordinate system.

--- RecFCCeeCalorimeter/src/components/CellPositionsHCalPhiThetaSegTool.h
+++ RecFCCeeCalorimeter/src/components/CellPositionsHCalPhiThetaSegTool.h
@@ -92,5 +92,7 @@ private:
   const std::vector<dd4hep::rec::LayeredCalorimeterStruct::Layer>* m_layersRetrieved = nullptr;
   /// for the HCal Endcap, one needs to provide the number of layers in each cylinder
   Gaudi::Property<std::vector<int>> m_numLayersHCalThreeParts{this, "numLayersHCalThreeParts", {6, 9, 22}};
+  /// Volume manager
+  dd4hep::VolumeManager m_volman;
 };
 #endif /* RECCALORIMETER_CellPositionsHCalPhiThetaSegTool_H */
