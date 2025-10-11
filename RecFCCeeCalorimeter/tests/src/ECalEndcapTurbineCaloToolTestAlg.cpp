9ceca77d5869ea64eca69759af33bf352ed2dfe2
sss <sss@karma>
2025-10-11 11:45:26 -0400

    Update tools to use K4_GAUDI_CHECK from k4FWCore rather than K4RECCALORIMETER_CHECK.

--- RecFCCeeCalorimeter/tests/src/ECalEndcapTurbineCaloToolTestAlg.cpp
+++ RecFCCeeCalorimeter/tests/src/ECalEndcapTurbineCaloToolTestAlg.cpp
@@ -7,7 +7,7 @@
 
 #undef NDEBUG
 #include "k4Interface/ICalorimeterTool.h"
-#include "RecCaloCommon/k4RecCalorimeter_check.h"
+#include "k4FWCore/k4_check.h"
 #include "GaudiKernel/Algorithm.h"
 #include "GaudiKernel/ToolHandle.h"
 #include <span>
@@ -36,23 +36,23 @@ DECLARE_COMPONENT(k4::recCalo::ECalEndcapTurbineCaloToolTestAlg);
 
 StatusCode ECalEndcapTurbineCaloToolTestAlg::initialize()
 {
-  K4RECCALORIMETER_CHECK( m_tool.retrieve() );
+  K4_GAUDI_CHECK( m_tool.retrieve() );
 
-  K4RECCALORIMETER_CHECK (m_tool->readoutName() == "ECalEndcapTurbine");
-  K4RECCALORIMETER_CHECK (m_tool->id() == 5);
+  K4_GAUDI_CHECK (m_tool->readoutName() == "ECalEndcapTurbine");
+  K4_GAUDI_CHECK (m_tool->id() == 5);
 
   std::span<const uint64_t> ids = m_tool->cellIDs();
   size_t ncells = ids.size();
-  K4RECCALORIMETER_CHECK (ncells == 1203200);
+  K4_GAUDI_CHECK (ncells == 1203200);
 
   std::unique_ptr<ICaloIndexer> indexer = m_tool->indexer();
-  K4RECCALORIMETER_CHECK (indexer != nullptr);
-  K4RECCALORIMETER_CHECK( indexer->detIDBits() == 4 );
+  K4_GAUDI_CHECK (indexer != nullptr);
+  K4_GAUDI_CHECK( indexer->detIDBits() == 4 );
 
-  K4RECCALORIMETER_CHECK (indexer->cellIDs().size() == ncells);
+  K4_GAUDI_CHECK (indexer->cellIDs().size() == ncells);
   for (size_t i = 0; i < ncells; i++) {
-    K4RECCALORIMETER_CHECK (ids[i] == indexer->cellIDs()[i]);
-    K4RECCALORIMETER_CHECK (indexer->index(ids[i]) == i);
+    K4_GAUDI_CHECK (ids[i] == indexer->cellIDs()[i]);
+    K4_GAUDI_CHECK (indexer->index(ids[i]) == i);
   }
 
   return StatusCode::SUCCESS;
--- RecFCChhCalorimeter/src/components/CellPositionsHCalBarrelNoSegTool.cpp
+++ RecFCChhCalorimeter/src/components/CellPositionsHCalBarrelNoSegTool.cpp
@@ -1,13 +1,13 @@
 #include "CellPositionsHCalBarrelNoSegTool.h"
-#include "RecCaloCommon/k4RecCalorimeter_check.h"
+#include "k4FWCore/k4_check.h"
 
 #include "edm4hep/CalorimeterHitCollection.h"
 
 DECLARE_COMPONENT(CellPositionsHCalBarrelNoSegTool)
 
 StatusCode CellPositionsHCalBarrelNoSegTool::initialize() {
-  K4RECCALORIMETER_CHECK( AlgTool::initialize() );
-  K4RECCALORIMETER_CHECK( m_geoSvc.retrieve() );
+  K4_GAUDI_CHECK( AlgTool::initialize() );
+  K4_GAUDI_CHECK( m_geoSvc.retrieve() );
 
   // get PhiEta segmentation
   m_segmentation = dynamic_cast<dd4hep::DDSegmentation::FCCSWGridPhiEta_k4geo*>(
