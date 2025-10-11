aff6c042890c2be333050864338efdbd36007f7d
sss <sss@karma>
2025-10-11 11:45:26 -0400

    Update tools to use K4_GAUDI_CHECK from k4FWCore rather than K4RECCALORIMETER_CHECK.

--- RecFCCeeCalorimeter/src/components/CellPositionsHCalPhiThetaSegTool.cpp
+++ RecFCCeeCalorimeter/src/components/CellPositionsHCalPhiThetaSegTool.cpp
@@ -1,5 +1,5 @@
 #include "CellPositionsHCalPhiThetaSegTool.h"
-#include "RecCaloCommon/k4RecCalorimeter_check.h"
+#include "k4FWCore/k4_check.h"
 
 #include "edm4hep/CalorimeterHitCollection.h"
 #include <DDRec/DetectorData.h>
@@ -9,8 +9,8 @@ using dd4hep::DetElement;
 DECLARE_COMPONENT(CellPositionsHCalPhiThetaSegTool)
 
 StatusCode CellPositionsHCalPhiThetaSegTool::initialize() {
-  K4RECCALORIMETER_CHECK( AlgTool::initialize() );
-  K4RECCALORIMETER_CHECK( m_geoSvc.retrieve() );
+  K4_GAUDI_CHECK( AlgTool::initialize() );
+  K4_GAUDI_CHECK( m_geoSvc.retrieve() );
 
   const dd4hep::Detector* detector = m_geoSvc->getDetector();
   if (!detector) {
