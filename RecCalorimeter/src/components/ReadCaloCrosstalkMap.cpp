93c8880b8aa0e0eb8e9239b7b60e63953da982b1
sss <sss@karma>
2025-10-11 11:45:26 -0400

    Update tools to use K4_GAUDI_CHECK from k4FWCore rather than K4RECCALORIMETER_CHECK.

--- RecCalorimeter/src/components/ReadCaloCrosstalkMap.cpp
+++ RecCalorimeter/src/components/ReadCaloCrosstalkMap.cpp
@@ -1,5 +1,6 @@
 #include "ReadCaloCrosstalkMap.h"
 #include "RecCaloCommon/k4RecCalorimeter_check.h"
+#include "k4FWCore/k4_check.h"
 
 #include "TBranch.h"
 #include "TFile.h"
@@ -21,7 +22,7 @@ StatusCode ReadCaloCrosstalkMap::initialize() {
 
   info() << "Loading crosstalk map..." << endmsg;
 
-  K4RECCALORIMETER_CHECK( AlgTool::initialize() );
+  K4_GAUDI_CHECK( AlgTool::initialize() );
 
   // Check if crosstalk file exists
   if (gSystem->AccessPathName(m_fileName.value().c_str())) {
