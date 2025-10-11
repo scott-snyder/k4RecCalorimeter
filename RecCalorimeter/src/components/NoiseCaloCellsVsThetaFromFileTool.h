7494eccae8ff85468b89160fcaa4f899c30083b6
sss <sss@karma>
2025-10-11 11:45:26 -0400

    Update tools to use K4_GAUDI_CHECK from k4FWCore rather than K4RECCALORIMETER_CHECK.

--- RecCalorimeter/src/components/NoiseCaloCellsVsThetaFromFileTool.h
+++ RecCalorimeter/src/components/NoiseCaloCellsVsThetaFromFileTool.h
@@ -13,7 +13,6 @@
 #include "k4Interface/ICellPositionsTool.h"
 #include "k4Interface/INoiseCaloCellsTool.h"
 #include "k4Interface/INoiseConstTool.h"
-#include "RecCaloCommon/k4RecCalorimeter_check.h"
 #include "edm4hep/EventHeaderCollection.h"
 #include "k4FWCore/DataHandle.h"
 #include "CLHEP/Random/Ranlux64Engine.h"
