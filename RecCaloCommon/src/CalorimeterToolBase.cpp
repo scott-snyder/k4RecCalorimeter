fb5266601e19257f71e79d2ea42f5fce5d8b9a5c
sss <sss@karma>
2025-10-23 11:45:40 -0400

    Refresh of cellIDs1

--- RecCaloCommon/src/CalorimeterToolBase.cpp
+++ RecCaloCommon/src/CalorimeterToolBase.cpp
@@ -7,7 +7,7 @@
 
 #include "RecCaloCommon/CalorimeterToolBase.h"
 #include "k4Interface/IGeoSvc.h"
-#include "k4FWCore/k4_check.h"
+#include "RecCaloCommon/k4RecCalorimeter_check.h"
 #include "DD4hep/Detector.h"
 #include <algorithm>
 
@@ -16,8 +16,8 @@
  */
 StatusCode CalorimeterToolBase::initialize()
 {
-  K4_GAUDI_CHECK( AlgTool::initialize() );
-  K4_GAUDI_CHECK( m_geoSvc.retrieve() );
+  K4RECCALORIMETER_CHECK( AlgTool::initialize() );
+  K4RECCALORIMETER_CHECK( m_geoSvc.retrieve() );
 
   // Look up the readout.
   if (!m_readoutName.empty()) {
