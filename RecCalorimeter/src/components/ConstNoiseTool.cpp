72d059903e13d48089907aa0594febb581af7b86
sss <sss@karma>
2025-10-30 16:05:03 -0400

    Refresh of noise2

--- RecCalorimeter/src/components/ConstNoiseTool.cpp
+++ RecCalorimeter/src/components/ConstNoiseTool.cpp
@@ -1,11 +1,11 @@
 #include "ConstNoiseTool.h"
+#include "RecCaloCommon/k4RecCalorimeter_check.h"
 
 // k4geo
 #include "detectorCommon/DetUtils_k4geo.h"
 
 // k4FWCore
 #include "k4Interface/IGeoSvc.h"
-#include "k4FWCore/k4_check.h"
 
 // DD4hep
 #include "DD4hep/Detector.h"
@@ -28,7 +28,7 @@ StatusCode ConstNoiseTool::initialize() {
     }
   }
 
-  K4_GAUDI_CHECK( m_geoSvc.retrieve() );
+  K4RECCALORIMETER_CHECK( m_geoSvc.retrieve() );
 
   // loop over the detectors
   for (size_t iDet = 0; iDet < m_detectors.size(); iDet++) {
@@ -49,7 +49,7 @@ StatusCode ConstNoiseTool::initialize() {
     }
   }
 
-  K4_GAUDI_CHECK( AlgTool::initialize() );
+  K4RECCALORIMETER_CHECK( AlgTool::initialize() );
 
   return StatusCode::SUCCESS;
 }
--- RecCalorimeter/src/components/ReadNoiseFromFileTool.cpp
+++ RecCalorimeter/src/components/ReadNoiseFromFileTool.cpp
@@ -1,11 +1,11 @@
 #include "ReadNoiseFromFileTool.h"
+#include "RecCaloCommon/k4RecCalorimeter_check.h"
 
 // k4geo
 #include "detectorCommon/DetUtils_k4geo.h"
 
 // k4FWCore
 #include "k4Interface/IGeoSvc.h"
-#include "k4FWCore/k4_check.h"
 
 // DD4hep
 #include "DD4hep/Detector.h"
@@ -21,7 +21,7 @@
 DECLARE_COMPONENT(ReadNoiseFromFileTool)
 
 StatusCode ReadNoiseFromFileTool::initialize() {
-  K4_GAUDI_CHECK( m_geoSvc.retrieve() );
+  K4RECCALORIMETER_CHECK( m_geoSvc.retrieve() );
 
   m_segmentation = dynamic_cast<dd4hep::DDSegmentation::FCCSWGridPhiEta_k4geo*>(
       m_geoSvc->getDetector()->readout(m_readoutName).segmentation().segmentation());
@@ -31,12 +31,12 @@ StatusCode ReadNoiseFromFileTool::initialize() {
   }
 
   // open and check file, read the histograms with noise constants
-  K4_GAUDI_CHECK( ReadNoiseFromFileTool::initNoiseFromFile() );
+  K4RECCALORIMETER_CHECK( ReadNoiseFromFileTool::initNoiseFromFile() );
 
   // Take readout bitfield decoder from GeoSvc
   m_decoder = m_geoSvc->getDetector()->readout(m_readoutName).idSpec().decoder();
 
-  K4_GAUDI_CHECK( AlgTool::initialize() );
+  K4RECCALORIMETER_CHECK( AlgTool::initialize() );
 
   return StatusCode::SUCCESS;
 }
