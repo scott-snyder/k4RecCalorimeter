886e0e5a82a6336ad6a8edf0e7bbe45e27bc133d
scott snyder <snyder@bnl.gov>
2025-11-26 16:20:51 -0500

    Remove/gate debug()/verbose() calls.
    Even if debug messages are suppressed, calling debug()<<... can take
    significant time when it's used on a hot path.  Remove some of those calls.

--- RecFCCeeCalorimeter/src/components/CaloTopoClusterFCCee.cpp
+++ RecFCCeeCalorimeter/src/components/CaloTopoClusterFCCee.cpp
@@ -268,19 +268,25 @@ CaloTopoClusterFCCee::findSeeds(const edm4hep::CalorimeterHitCollection* allCell
 
   for (const auto& cell : *allCells) {
 
-    verbose() << "cellID   = " << cell.getCellID() << endmsg;
+    if (this->msgLevel(MSG::VERBOSE)) {
+      verbose() << "cellID   = " << cell.getCellID() << endmsg;
+    }
 
     // retrieve the noise const and offset assigned to cell
     auto [rms, offset] = m_noiseTool->getNoisePerCell(cell.getCellID());
     double threshold = offset + rms * m_seedSigma;
 
-    debug() << "======================================" << endmsg;
-    debug() << "noise offset    = " << offset << " GeV " << endmsg;
-    debug() << "noise rms       = " << rms << " GeV " << endmsg;
-    debug() << "seed threshold  = " << threshold << " GeV " << endmsg;
-    debug() << "======================================" << endmsg;
+    if (this->msgLevel(MSG::DEBUG)) {
+      debug() << "======================================" << endmsg;
+      debug() << "noise offset    = " << offset << " GeV " << endmsg;
+      debug() << "noise rms       = " << rms << " GeV " << endmsg;
+      debug() << "seed threshold  = " << threshold << " GeV " << endmsg;
+      debug() << "======================================" << endmsg;
+    }
     if (std::fabs(cell.getEnergy()) > threshold) {
-      debug() << "Found seed" << endmsg;
+      if (this->msgLevel(MSG::DEBUG)) {
+        debug() << "Found seed" << endmsg;
+      }
       seedCellsVec.emplace_back(cell);
     }
   }
