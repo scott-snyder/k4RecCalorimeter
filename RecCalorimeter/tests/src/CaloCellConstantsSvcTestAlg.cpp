a740a843b9adbd4af0fc1551efc9ecaf2ba06626
sss <sss@karma>
2026-01-25 18:03:45 -0500

    Numerous tools deal with constants defined for each calorimeter cell. For highly-granular calorimeters with many cells, such as the Allegro ECal, this can consume a significant amount of memory.  But then if one creates multiple instances of these tools --- perhaps to explore the effects of different configurations --- then these data may be duplicated, effectively wasting the memory allocated by the duplicates.  This service provides a simple key/value registry allowing the data to be stored outside of the tools themselves, and thus providing the possibility of sharing.
    This can be thought of as supplying something akin to the ATLAS
    detector store.  In principle, it would be nice to store such objects
    the same manner as event data.  But podio is not so suitable, as it
    does not allow for storing arbitrary types, and in general does not
    really seem intended for the storage of data which is not meant
    to be persistent.

---
+++ RecCalorimeter/tests/src/CaloCellConstantsSvcTestAlg.cpp
@@ -0,0 +1,64 @@
+/**
+ * @file RecCalorimeter/tests/src/CaloCellConstantsSvc.cpp
+ * @author scott snyder <snyder@bnl.gov>
+ * @date Jan, 2026
+ * @brief Test for CaloCellConstantsSvc
+ */
+
+#include "RecCaloCommon/ICaloCellConstantsSvc.h"
+#include "RecCaloCommon/k4RecCalorimeter_check.h"
+#include "GaudiKernel/Algorithm.h"
+#include "GaudiKernel/ServiceHandle.h"
+
+
+namespace k4::recCalo {
+
+
+class CaloCellConstantsSvcTestAlg
+  : public Algorithm
+{
+public:
+  using Algorithm::Algorithm;
+
+  virtual StatusCode initialize() override;
+  virtual StatusCode execute() override;
+
+private:
+  ServiceHandle<ICaloCellConstantsSvc> m_svc
+  { this, "CaloCellConstantsSvc", "k4::recCalo::CaloCellConstantsSvc", "" };
+};
+
+
+DECLARE_COMPONENT(k4::recCalo::CaloCellConstantsSvcTestAlg);
+
+
+StatusCode CaloCellConstantsSvcTestAlg::initialize()
+{
+  K4RECCALORIMETER_CHECK( m_svc.retrieve() );
+
+  using payload_t = std::vector<int>;
+  K4RECCALORIMETER_CHECK (!m_svc->getObj<payload_t> ("test"));
+  payload_t v1 {1, 2, 3};
+  payload_t v2 (v1);
+  K4RECCALORIMETER_CHECK (m_svc->putObj ("test", std::move(v2)));
+  K4RECCALORIMETER_CHECK (v2.empty());
+  const payload_t* v3 = nullptr;
+  K4RECCALORIMETER_CHECK ((v3 = m_svc->getObj<payload_t> ("test")));
+  K4RECCALORIMETER_CHECK (*v3 == v1);
+  K4RECCALORIMETER_CHECK (!m_svc->getObj<payload_t> ("test2"));
+  K4RECCALORIMETER_CHECK (!m_svc->getObj<std::vector<float> > ("test1"));
+  K4RECCALORIMETER_CHECK (!m_svc->putObj ("test", std::move(v2)));
+  K4RECCALORIMETER_CHECK ((v3 = m_svc->getObj<payload_t> ("test")));
+  K4RECCALORIMETER_CHECK (*v3 == v1);
+
+  return StatusCode::SUCCESS;
+}
+
+
+StatusCode CaloCellConstantsSvcTestAlg::execute()
+{
+  return StatusCode::SUCCESS;
+}
+
+
+} // namespace k4::recCalo
