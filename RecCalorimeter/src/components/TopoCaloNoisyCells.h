4b7227c11dc068c99d5a973d79618905366ac8e3
scott snyder <snyder@bnl.gov>
2026-01-26 16:04:40 -0500

    More efficient data storage for TopoCaloNoisyCells.

--- RecCalorimeter/src/components/TopoCaloNoisyCells.h
+++ RecCalorimeter/src/components/TopoCaloNoisyCells.h
@@ -6,8 +6,12 @@
 
 // k4FWCore
 #include "k4Interface/INoiseConstTool.h"
+#include "RecCaloCommon/ICaloCellConstantsSvc.h"
+#include "RecCaloCommon/ICaloCellIndexerSvc.h"
+#include <utility>
 
 class IGeoSvc;
+class TFile;
 
 /** @class TopoCaloNoisyCells Reconstruction/RecCalorimeter/src/components/TopoCaloNoisyCells.h
  *TopoCaloNoisyCells.h
@@ -50,7 +54,21 @@ private:
   /// Name
   Gaudi::Property<std::string> m_fileName{this, "fileName",
                                           "/afs/cern.ch/user/c/cneubuse/public/FCChh/cellNoise_map_segHcal.root"};
-  std::unordered_map<uint64_t, std::pair<double, double>> m_map;
+  ServiceHandle<k4::recCalo::ICaloCellConstantsSvc> m_constantsSvc
+  { this, "CaloCellConstantsSvc", "k4::recCalo::CaloCellConstantsSvc", "" };
+  ServiceHandle<k4::recCalo::ICaloCellIndexerSvc> m_indexerSvc
+  { this, "CaloCellIndexerSvc", "k4::recCalo::CaloCellIndexerSvc", "" };
+
+  /// xxx fixme
+  Gaudi::Property<int> m_detID {this, "DetID", 4};
+
+  // rms, offset
+  using NoiseData = std::vector<std::pair<double, double> >;
+  const NoiseData* m_data = nullptr;
+
+  const k4::recCalo::ICaloIndexer* m_indexer = nullptr;
+
+  NoiseData readData (TFile& inFile) const;
 };
 
 #endif /* RECCALORIMETER_TOPOCALONOISYCELLS_H */
