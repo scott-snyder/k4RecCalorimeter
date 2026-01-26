a5129a40c9f645e1acc1e18382b6b2b0624fd296
scott snyder <snyder@bnl.gov>
2026-01-26 16:04:40 -0500

    More efficient data storage for TopoCaloNoisyCells.

--- RecCalorimeter/src/components/TopoCaloNoisyCells.cpp
+++ RecCalorimeter/src/components/TopoCaloNoisyCells.cpp
@@ -1,37 +1,65 @@
 #include "TopoCaloNoisyCells.h"
 #include "RecCaloCommon/k4RecCalorimeter_check.h"
+#include "DDSegmentation/BitFieldCoder.h"
 
 #include "TBranch.h"
 #include "TFile.h"
 #include "TSystem.h"
 #include "TTree.h"
 
+#include <algorithm>
+
 DECLARE_COMPONENT(TopoCaloNoisyCells)
 
 StatusCode TopoCaloNoisyCells::initialize() {
   K4RECCALORIMETER_CHECK( AlgTool::initialize() );
+  K4RECCALORIMETER_CHECK( m_constantsSvc.retrieve() );
+  K4RECCALORIMETER_CHECK( m_indexerSvc.retrieve() );
 
-  // Check if file exists
-  if (m_fileName.empty()) {
-    error() << "Name of the file with the noisy cells not provided!" << endmsg;
-    return StatusCode::FAILURE;
+  // setup system decoder
+  m_decoder = std::make_unique<dd4hep::DDSegmentation::BitFieldCoder>(m_systemEncoding);
+  m_indexSystem = m_decoder->index("system");
+
+  m_data = m_constantsSvc->getObj<NoiseData> (m_fileName);
+  if (!m_data) {
+    // Check if file exists
+    if (m_fileName.empty()) {
+      error() << "Name of the file with the noisy cells not provided!" << endmsg;
+      return StatusCode::FAILURE;
+    }
+    if (gSystem->AccessPathName(m_fileName.value().c_str())) {
+      error() << "Provided file with the noisy cells not found!" << endmsg;
+      error() << "File path: " << m_fileName.value() << endmsg;
+      return StatusCode::FAILURE;
+    }
+    std::unique_ptr<TFile> inFile(TFile::Open(m_fileName.value().c_str(), "READ"));
+    if (inFile->IsZombie()) {
+      error() << "Unable to open the file with the noisy cells!" << endmsg;
+      error() << "File path: " << m_fileName.value() << endmsg;
+      return StatusCode::FAILURE;
+    } else {
+      info() << "Using the following file with the noisy cells: " << m_fileName.value() << endmsg;
+    }
+
+    NoiseData data = readData (*inFile);
+    K4RECCALORIMETER_CHECK( m_constantsSvc->putObj (m_fileName, std::move (data)) );
+    m_data = m_constantsSvc->getObj<NoiseData> (m_fileName);
+    K4RECCALORIMETER_CHECK( m_data != nullptr );
   }
-  if (gSystem->AccessPathName(m_fileName.value().c_str())) {
-    error() << "Provided file with the noisy cells not found!" << endmsg;
-    error() << "File path: " << m_fileName.value() << endmsg;
-    return StatusCode::FAILURE;
-  }
-  std::unique_ptr<TFile> inFile(TFile::Open(m_fileName.value().c_str(), "READ"));
-  if (inFile->IsZombie()) {
-    error() << "Unable to open the file with the noisy cells!" << endmsg;
-    error() << "File path: " << m_fileName.value() << endmsg;
-    return StatusCode::FAILURE;
-  } else {
-    info() << "Using the following file with the noisy cells: " << m_fileName.value() << endmsg;
-  }
+
+  m_indexer = m_data->m_indexer;
+  K4RECCALORIMETER_CHECK( m_indexer != nullptr );
+
+  return StatusCode::SUCCESS;
+}
+
+
+auto TopoCaloNoisyCells::readData (TFile& inFile) const -> NoiseData
+{
+  NoiseData data;
 
   TTree* tree = nullptr;
-  inFile->GetObject("noisyCells", tree);
+  inFile.GetObject("noisyCells", tree);
   ULong64_t readCellId;
   double readNoisyCells;
   double readNoisyCellsOffset;
@@ -39,42 +67,56 @@ StatusCode TopoCaloNoisyCells::initialize() {
   tree->SetBranchAddress("noiseLevel",
                          &readNoisyCells); // would be better to call branch noiseRMS rather than noiseLevel
   tree->SetBranchAddress("noiseOffset", &readNoisyCellsOffset);
+
+  data.m_noise.resize (data.m_indexer->cellIDs().size());
+
+  // First find the set of detIDs in order to get the proper indexer.
+  // Just use a vector, since we only expect a handful.
+  std::vector<int> detIDs;
+  tree->SetBranchStatus ("*", 0);
+  tree->SetBranchStatus ("cellId", 1);
   for (uint i = 0; i < tree->GetEntries(); i++) {
     tree->GetEntry(i);
-    m_map.insert(std::pair<uint64_t, std::pair<double, double>>(readCellId,
-                                                                std::make_pair(readNoisyCells, readNoisyCellsOffset)));
+    int detID = m_decoder->get (readCellId, m_indexSystem);
+    if (std::ranges::find (detIDs, detID) != detIDs.end()) {
+      detIDs.push_back (detID);
+    }
+  }
+  std::ranges::sort (detIDs);
+  data.m_indexer = m_indexerSvc->indexer (detIDs);
+  if (!data.m_indexer) return data;
+
+  tree->SetBranchStatus ("*", 1);
+  for (uint i = 0; i < tree->GetEntries(); i++) {
+    tree->GetEntry(i);
+    unsigned ndx = data.m_indexer->index (readCellId);
+    data.m_noise.at(ndx) = std::make_pair (readNoisyCells, readNoisyCellsOffset);
   }
   delete tree;
-  inFile->Close();
+  inFile.Close();
 
-  return StatusCode::SUCCESS;
+  return data;
 }
+
 
 double TopoCaloNoisyCells::getNoiseRMSPerCell(uint64_t aCellId) const
 {
-  auto it = m_map.find(aCellId);
-  if (it != m_map.end()) {
-    return it->second.first;
-  }
-  return 0;
+  unsigned ndx = m_indexer->index (aCellId);
+  return m_data->m_noise.at(ndx).first;
 }
 
 
 double TopoCaloNoisyCells::getNoiseOffsetPerCell(uint64_t aCellId) const
 {
-  auto it = m_map.find(aCellId);
-  if (it != m_map.end()) {
-    return it->second.second;
-  }
-  return 0;
+  unsigned ndx = m_indexer->index (aCellId);
+  return m_data->m_noise.at(ndx).second;
 }
 
-std::pair<double,double>
+
+std::pair<double, double>
 TopoCaloNoisyCells::getNoisePerCell(uint64_t aCellId) const
 {
-  auto it = m_map.find(aCellId);
-  if (it != m_map.end()) {
-    return it->second;
-  }
-  return std::make_pair(0., 0.);
+  unsigned ndx = m_indexer->index (aCellId);
+  return m_data->m_noise.at(ndx);
 }
+
