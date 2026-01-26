7d7c92d85bf93fc7333c3ad8c57ee42f166cd454
scott snyder <snyder@bnl.gov>
2026-01-26 16:07:03 -0500

    createCaloCell2-wip

--- RecCalorimeter/src/components/CreateCaloCells.h
+++ RecCalorimeter/src/components/CreateCaloCells.h
@@ -103,22 +103,22 @@ private:
 
   struct CellsFullIndex
   {
-    CellsFullIndex (const CellsIndexMap_t& cellsIndexMap)
-      : m_cellsIndexMap (cellsIndexMap),
-        m_indices (cellsIndexMap.size(), {INVALID, INVALID})
+    CellsFullIndex (const ICalorimeterTool& geoTool, size_t ncells)
+      : m_geoTool (geoTool)
     {
+      m_indices.reserve (ncells);
+      for (size_t i = 0; i < ncells; i++) {
+        m_indices.emplace_back (i, INVALID);
+      }
     }
 
     CellsIndexPair_t& index (uint64_t cellid)
     {
-      auto it = m_cellsIndexMap.find (cellid);
-      if (it == m_cellsIndexMap.end()) {
-        throw std::out_of_range ("bad cellid");
-      }
-      return m_indices.at (it->second);
+      unsigned ndx = m_geoTool.index (cellid);
+      return m_indices.at (ndx);
     }
 
-    const CellsIndexMap_t& m_cellsIndexMap;
+    const ICalorimeterTool& m_geoTool;
     std::vector<CellsIndexPair_t> m_indices;
   };
 
@@ -146,10 +146,10 @@ private:
 
   struct CellsIndex
   {
-    CellsIndex (const CellsIndexMap_t& cellsIndexMap)
+    CellsIndex (const ICalorimeterTool* geoTool, size_t ncells)
     {
-      if (!cellsIndexMap.empty()) {
-        m_indices.emplace<1> (cellsIndexMap);
+      if (geoTool) {
+        m_indices.emplace<1> (*geoTool, ncells);
       }
       else {
         m_indices.emplace<2>();
@@ -187,6 +187,9 @@ private:
 
     std::variant<int, CellsFullIndex, CellsSparseIndex> m_indices;
   };
+
+  void addCrosstalk (CellsInfo& cells,
+                     CellsIndex& cellsIndex) const;
 
   /// Handle for the calorimeter cells crosstalk tool
   ToolHandle<ICaloReadCrosstalkMap> m_crosstalksTool
@@ -248,9 +251,7 @@ private:
   /// Pointer to the geometry service
   ServiceHandle<IGeoSvc> m_geoSvc;
   dd4hep::VolumeManager m_volman;
-  /// Map of cell IDs to cell indices.
-  /// This assigns to each cell a dense index in the range 0..ncells-1.
-  CellsIndexMap_t m_cellsIndexMap;
+  std::span<const uint64_t> m_cellIDs;
 };
 
 #endif /* RECCALORIMETER_CREATECALOCELLS_H */
