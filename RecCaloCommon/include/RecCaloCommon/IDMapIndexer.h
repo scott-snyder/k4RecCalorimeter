0e66b458e99a4a6ed5396a92f9c798ae035a0bb3
sss <sss@karma>
2026-01-28 15:02:00 -0500

    Add IDMapIndexer
    Add a concrete implementation of ICaloIndexer using IDMap.

---
+++ RecCaloCommon/include/RecCaloCommon/IDMapIndexer.h
@@ -0,0 +1,147 @@
+// This file's extension implies that it's C, but it's really -*- C++ -*-.
+/**
+ * @file RecCaloCommon/IDMapIndexer.h
+ * @author scott snyder <snyder@bnl.gov>
+ * @date Jan, 2026
+ * @brief Implementation of ICaloIndexer using IDMap.
+ */
+
+
+#ifndef RECCALOCOMMON_IDMAPINDEXER_H
+#define RECCALOCOMMON_IDMAPINDEXER_H
+
+
+#include "RecCaloCommon/IDMap.h"
+#include "k4Interface/ICaloIndexer.h"
+
+
+namespace k4::recCalo {
+
+
+/**
+ * @brief Implementation of ICaloIndexer using IDMap.
+ *
+ * This is an implementation of @c ICaloIndexer for a specific subdetector
+ * based on @c IDMap with a fixed number of fields.
+ */
+template <unsigned NFIELDS>
+class IDMapIndexer : public ICaloIndexer
+{
+public:
+  /// Type of an index.
+  using index_t = ICaloIndexer::index_t;
+
+  /// Flag indicating an invalid index.
+  static constexpr index_t INVALID = static_cast<index_t>(-1);
+
+  /// Type of the mapping.
+  using IDMap_t = IDMapN<index_t, NFIELDS>;
+
+  /// Type describing a field; used to initialize the mapping.
+  using FieldDesc_t = typename IDMap_t::FieldDesc_t;
+
+
+  /**
+   * @brief Constructor.
+   * @param detID ID of the detector that we index.
+   * @param fields Set of fields to use from the identifiers.
+   *               Must be sufficient to make identifiers unique.
+   *               For best results, should be listed in order of increasing
+   *               bit width.
+   *               The size must be exactly @c NFIELDS.
+   * @param ids Set of all identifiers to be indexed.
+   *            Should be sorted for best results.
+   * param sizeHint If non-zero, this is an estimate of the total size,
+   *                in bytes, required by this mapping.  This will be
+   *                used to reserve an appropriate size for the data vector.
+   */
+  IDMapIndexer (int detID,
+                std::span<const FieldDesc_t> fields,
+                std::span<const uint64_t> ids,
+                size_t sizeHint = 0);
+
+
+  /**
+   * @brief Return the index of an identifier.
+   * @param id The identifier to look for.
+   *
+   * Returns the index of @c id in @c cellIDs(), or @c INVALID.
+   */
+  virtual index_t index (uint64_t id) const override final;
+
+
+  /**
+   * @brief Return the set of all identifiers that we index.
+   */
+  virtual std::span<const uint64_t> cellIDs() const override final;
+
+
+  /**
+   * @brief Return the IDs of the detector(s) that we index.
+   */
+  virtual std::span<const int> detIDs() const override final;
+
+  
+private:
+  /// The mapping.
+  IDMap_t m_map;
+
+  /// The ID of the detector we index.
+  int m_detID;
+
+  /// The set of cells that we index.
+  std::span<const uint64_t> m_cellIDs;
+};
+
+
+template <unsigned NFIELDS>
+IDMapIndexer<NFIELDS>::IDMapIndexer (int detID,
+                                     std::span<const FieldDesc_t> fields,
+                                     std::span<const uint64_t> ids,
+                                     size_t sizeHint /*= 0*/)
+  : m_map (fields, INVALID,ids,
+           [](size_t i) { return i; },
+           sizeHint),
+    m_detID (detID),
+    m_cellIDs (ids)
+{
+}
+
+
+/**
+ * @brief Return the index of an identifier.
+ */
+template <unsigned NFIELDS>
+inline
+auto IDMapIndexer<NFIELDS>::index (uint64_t id) const -> index_t
+{
+  return m_map.lookup (id);
+}
+
+
+/**
+ * @brief Return the set of all identifiers that we index.
+ */
+template <unsigned NFIELDS>
+inline
+std::span<const uint64_t> IDMapIndexer<NFIELDS>::cellIDs() const
+{
+  return m_cellIDs;
+}
+
+
+/**
+ * @brief Return the IDs of the detector(s) that we index.
+ */
+template <unsigned NFIELDS>
+inline
+std::span<const int> IDMapIndexer<NFIELDS>::detIDs() const
+{
+  return std::span<const int> (&m_detID, 1);
+}
+
+
+} // namespace k4::recCalo
+
+
+#endif // not RECCALOCOMMON_IDMAPINDEXER_H
