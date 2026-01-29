9e771ffdbf6d5fd5b9427450a83d5b47682b6ad9
sss <sss@karma>
2026-01-29 15:07:35 -0500

    caloCellIndexerSvc

---
+++ RecCalorimeter/tests/src/CaloCellIndexerSvcTestAlg.cpp
@@ -0,0 +1,65 @@
+/**
+ * @file RecCalorimeter/tests/src/CaloCellIndexerSvcTestAlg.cpp
+ * @author scott snyder <snyder@bnl.gov>
+ * @date Jan, 2026
+ * @brief Test for CaloCellIndexerSvc
+ */
+
+
+#include "RecCaloCommon/ICaloCellIndexerSvc.h"
+#include "RecCaloCommon/k4RecCalorimeter_check.h"
+#include "GaudiKernel/Algorithm.h"
+#include "GaudiKernel/ServiceHandle.h"
+#include <span>
+
+
+namespace k4::recCalo {
+
+
+class CaloCellIndexerSvcTestAlg
+  : public Algorithm
+{
+public:
+  using Algorithm::Algorithm;
+
+  virtual StatusCode initialize() override;
+  virtual StatusCode execute() override;
+
+private:
+  ServiceHandle<ICaloCellIndexerSvc> m_svc
+  { this, "CaloCellIndexerSvc", "k4::recCalo::CaloCellIndexerSvc", "" };
+
+  Gaudi::Property<int> m_detID
+  { this, "DetID", 4, "" };
+};
+
+
+DECLARE_COMPONENT(k4::recCalo::CaloCellIndexerSvcTestAlg);
+
+
+StatusCode CaloCellIndexerSvcTestAlg::initialize()
+{
+  K4RECCALORIMETER_CHECK( m_svc.retrieve() );
+
+  K4RECCALORIMETER_CHECK( m_svc->indexer (999) == nullptr );
+
+  const ICaloIndexer* indexer = m_svc->indexer (m_detID);
+  K4RECCALORIMETER_CHECK( indexer != nullptr );
+
+  std::span<const uint64_t> ids = indexer->cellIDs();
+  for (size_t i = 0; i < ids.size(); ++i) {
+    K4RECCALORIMETER_CHECK( indexer->index(ids[i]) == i );
+  }
+
+  return StatusCode::SUCCESS;
+}
+
+
+StatusCode CaloCellIndexerSvcTestAlg::execute()
+{
+  return StatusCode::SUCCESS;
+}
+
+
+} // namespace k4::recCalo
+
