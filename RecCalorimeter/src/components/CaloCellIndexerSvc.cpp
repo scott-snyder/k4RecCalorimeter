58d8f9497f688b0f7d7afb2980a616252d40740d
sss <sss@karma>
2026-01-29 15:07:35 -0500

    caloCellIndexerSvc

---
+++ RecCalorimeter/src/components/CaloCellIndexerSvc.cpp
@@ -0,0 +1,63 @@
+/**
+ * @file CaloCellIndexerSvc.cpp
+ * @author scott snyder <snyder@bnl.gov>
+ * @date Jan, 2026
+ * @brief Holder for calorimeter cell indexers.
+ */
+
+
+#include "CaloCellIndexerSvc.h"
+#include "RecCaloCommon/k4RecCalorimeter_check.h"
+
+
+DECLARE_COMPONENT(k4::recCalo::CaloCellIndexerSvc);
+
+
+namespace k4::recCalo {
+
+
+/**
+ * @brief Gaudi initialize method.
+ */
+StatusCode CaloCellIndexerSvc::initialize()
+{
+  K4RECCALORIMETER_CHECK( Service::initialize() );
+  K4RECCALORIMETER_CHECK( m_geoTools.retrieve() );
+
+  // Make indexers for all tools that support it.
+  for (ToolHandle<ICalorimeterTool>& tool : m_geoTools) {
+    std::unique_ptr<k4::recCalo::ICaloIndexer> indexer = tool->indexer();
+    if (indexer) {
+      int detid = tool->id();
+      if (m_indexers.size() <= static_cast<size_t>(detid)) {
+        m_indexers.resize (detid+1);
+      }
+      if (m_indexers[detid]) {
+        error() << "Duplicated detector ID " << detid << endmsg;
+        return StatusCode::FAILURE;
+      }
+      m_indexers[detid] = std::move(indexer);
+    }
+  }
+
+  return StatusCode::SUCCESS;
+}
+
+
+/**
+ * @brief Return indexer for a given subdetector.
+ * @param detID Subdetector ID for the desired indexer.
+ *
+ * Returns a pointer to the indexer or nullptr if there isn't one defined.
+ */
+const k4::recCalo::ICaloIndexer* CaloCellIndexerSvc::indexer (int detID) const
+{
+  if (static_cast<size_t>(detID) < m_indexers.size()) {
+    return m_indexers[detID].get();
+  }
+  return nullptr;
+}
+
+
+} // namespace k4::recCalo
+
