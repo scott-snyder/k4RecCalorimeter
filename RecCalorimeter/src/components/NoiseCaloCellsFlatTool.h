6f5cf85d18089e1a6385d78ba93cc8b509716b14
sss <sss@karma>
2026-02-03 18:26:16 -0500

    Make noise reproducible.

--- RecCalorimeter/src/components/NoiseCaloCellsFlatTool.h
+++ RecCalorimeter/src/components/NoiseCaloCellsFlatTool.h
@@ -5,6 +5,10 @@
 #include "GaudiKernel/AlgTool.h"
 #include "GaudiKernel/IRndmGenSvc.h"
 #include "GaudiKernel/RndmGenerators.h"
+#include "edm4hep/EventHeaderCollection.h"
+#include "k4FWCore/DataHandle.h"
+#include "CLHEP/Random/Ranlux64Engine.h"
+#include "CLHEP/Random/RandGauss.h"
 
 // Interfaces
 #include "RecCaloCommon/INoiseCaloCellsTool.h"
@@ -48,7 +52,7 @@ public:
 
 private:
   template <typename C>
-  void addRandomCellNoiseT(C& aCells) const;
+  void addRandomCellNoiseT (C& aCells, CLHEP::RandGauss& r) const;
   template <typename C>
   void filterCellNoiseT(C& aCells) const;
 
@@ -63,6 +67,10 @@ private:
   SmartIF<IRndmGenSvc> m_randSvc;
   /// Gaussian random number generator used for smearing with a constant resolution (m_sigma)
   Rndm::Numbers m_gauss;
+
+  mutable k4FWCore::DataHandle<edm4hep::EventHeaderCollection> m_header{"EventHeader", Gaudi::DataHandle::Reader, this};
+
+  void initEvent (CLHEP::Ranlux64Engine& e) const;
 };
 
 #endif /* RECCALORIMETER_NOISECALOCELLSFLATTOOL_H */
