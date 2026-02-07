f21005724dc1c0c9dfa00915b3308e8ff601e39e
scott snyder <snyder@bnl.gov>
2026-02-07 16:25:26 -0500

    Implement HCalPhiThetaCaloTool::indexer().
    Implement indexer() for the Allegro hcal, using IDMapIndexer.
    Also add a simple test.

---
+++ RecFCCeeCalorimeter/tests/options/HCalPhiThetaCaloTool_test.py
@@ -0,0 +1,29 @@
+#
+# File: RecFCCeeCalorimeter/tests/options/HCalPhiThetaCaloTool_test.py
+# Author: scott snyder <snyder@bnl.gov>
+# Date: Feb, 2026
+# Purpose: Test for HCalPhiThetaCaloTool
+#
+
+import os
+import Configurables as C
+
+compactFile = 'ALLEGRO_o1_v03.xml'
+pathToDetector = os.environ.get('K4GEO','') + '/FCCee/ALLEGRO/compact/' + os.path.splitext(compactFile)[0]
+
+geoSvc = C.GeoSvc('GeoSvc',
+                  detectors = [os.path.join (pathToDetector, compactFile)])
+
+hcalBarrelTool = C.HCalPhiThetaCaloTool \
+    ('hcalBarrelGeometryTool',
+     readoutName = 'HCalBarrelReadout')
+hcalEndcapTool = C.HCalPhiThetaCaloTool \
+    ('hcalEndcapGeometryTool',
+     readoutName = 'HCalEndcapReadout')
+
+
+appmgr = C.ApplicationMgr \
+    (TopAlg = [C.k4__recCalo__HCalPhiThetaCaloToolTestAlg
+               (HCalBarrelTool = hcalBarrelTool,
+                HCalEndcapTool = hcalEndcapTool)],
+     ExtSvc = [geoSvc])
