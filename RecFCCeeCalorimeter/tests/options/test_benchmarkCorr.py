a82e1737e8a0c4ea922e9bf2e4348a5e15dfb53c
sss <sss@karma>
2026-02-12 05:33:01 -0500

    More efficienct calorimeter cells reconstruction + positioning flexibility.

--- RecFCCeeCalorimeter/tests/options/test_benchmarkCorr.py
+++ RecFCCeeCalorimeter/tests/options/test_benchmarkCorr.py
@@ -227,7 +227,7 @@ createEcalBarrelCells = CreateCaloCells(
     addCrosstalk=False,
     addCellNoise=False,
     filterCellNoise=False,
-    addPosition=True,
+    addPosition=False,
     OutputLevel=INFO,
     hits=ecalBarrelReadoutName,
     cells=ecalBarrelCellsName,
@@ -284,6 +284,11 @@ createEcalEndcapPositionedCells.hits.Path = ecalEndcapCellsName
 createEcalEndcapPositionedCells.positionedHits.Path = ecalEndcapPositionedCellsName
 
 if runHCal:
+    from Configurables import CellPositionsHCalPhiThetaSegTool
+    hcalBarrelCellPosTool = CellPositionsHCalPhiThetaSegTool('hcalBarrelCellPosTool',
+                                                             readoutName=hcalBarrelReadoutName,
+                                                             detectorName='HCalBarrel')
+
     # Create cells in HCal
     # 1 - merge hits into cells with the default readout
     hcalBarrelCellsName = "HCalBarrelCells"
@@ -293,7 +298,7 @@ if runHCal:
         calibTool=calibHcells,
         addCellNoise=False,
         filterCellNoise=False,
-        addPosition=True,
+        addPosition=False,
         hits=hcalBarrelReadoutName,
         cells=hcalBarrelCellsName,
         OutputLevel=INFO,
