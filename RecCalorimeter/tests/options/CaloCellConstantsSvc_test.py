4a8705a04c27e190d187479710ccb54c79675ee4
sss <sss@karma>
2026-02-23 21:16:08 -0500

    test debugging

--- RecCalorimeter/tests/options/CaloCellConstantsSvc_test.py
+++ RecCalorimeter/tests/options/CaloCellConstantsSvc_test.py
@@ -5,6 +5,22 @@
 # Purpose: Test for CaloCellConstantsSvc
 #
 
+import os, sys
+print ('--- cwd ' + os.getcwd())
+print ('--- LD_LIBRARY_PATH ' + os.environ['LD_LIBRARY_PATH'], flush=True)
+for p in os.environ['LD_LIBRARY_PATH'].split(':'):
+    print('--- path ' + p, flush=True)
+    os.system ('ls ' + p)
+    pp = os.path.join (p, 'k4RecCalorimeter.confdb')
+    if os.path.exists (pp):
+        print('--- file ' + pp, flush=True)
+        os.system ('cat ' + pp)
+    pp = os.path.join (p, 'k4RecCalorimeter.components')
+    if os.path.exists (pp):
+        print('--- file ' + pp, flush=True)
+        os.system ('cat ' + pp)
+
 import Configurables as C
 appmgr = C.ApplicationMgr()
 appmgr.TopAlg += [C.k4__recCalo__CaloCellConstantsSvcTestAlg()]
+sys.exit(1)
