41790dfe51ba9341807de7ce479f89b52d441d4a
sss <sss@karma>
2026-02-25 15:36:20 -0500

    Do not clone k4geo in tests.
    Use the installed version instead.

--- RecCalorimeter/tests/scripts/testSiPM.sh
+++ RecCalorimeter/tests/scripts/testSiPM.sh
@@ -3,12 +3,6 @@
 SOURCE_DIR=`dirname $0`
 workdir=$PWD
 
-git clone --no-checkout https://github.com/key4hep/k4geo
-cd k4geo
-git checkout HEAD example
-cd $workdir
-
-ddsim --steeringFile=k4geo/example/SteeringFile_IDEA_o1_v03.py --compactFile=$K4GEO/FCCee/IDEA/compact/IDEA_o1_v03/IDEA_o1_v03.xml -G --gun.distribution uniform --gun.particle e- --random.seed 1234567 --numberOfEvents=5
+ddsim --steeringFile=$K4GEO/example/SteeringFile_IDEA_o1_v03.py --compactFile=$K4GEO/FCCee/IDEA/compact/IDEA_o1_v03/IDEA_o1_v03.xml -G --gun.distribution uniform --gun.particle e- --random.seed 1234567 --numberOfEvents=5
 
 k4run $SOURCE_DIR/../options/runSiPMDualReadout.py
-
