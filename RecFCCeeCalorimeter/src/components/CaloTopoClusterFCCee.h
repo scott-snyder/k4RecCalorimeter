4bfbbe8be2a8be8715c27e2201e941355fbc4ff1
sss <sss@karma>
2026-03-27 11:38:53 -0400

    Move INoiseCaloCellsTool and INoiesContTool here from k4FWCore.

--- RecFCCeeCalorimeter/src/components/CaloTopoClusterFCCee.h
+++ RecFCCeeCalorimeter/src/components/CaloTopoClusterFCCee.h
@@ -17,7 +17,7 @@
 #include "k4FWCore/DataHandle.h"
 #include "RecCaloCommon/ICaloReadNeighboursMap.h"
 #include "k4Interface/IGeoSvc.h"
-#include "k4Interface/INoiseConstTool.h"
+#include "RecCaloCommon/INoiseConstTool.h"
 
 // EDM4HEP
 namespace edm4hep {
@@ -118,7 +118,7 @@ private:
     this, "calorimeterIDs", {}, "Corresponding list of calorimeter IDs"};
 
   /// Handle for the cells noise tool
-  mutable ToolHandle<INoiseConstTool> m_noiseTool{"TopoCaloNoisyCells", this};
+  mutable ToolHandle<k4::recCalo::INoiseConstTool> m_noiseTool{"TopoCaloNoisyCells", this};
   /// Handle for neighbours tool
   mutable ToolHandle<k4::recCalo::ICaloReadNeighboursMap> m_neighboursTool{"TopoCaloNeighbours", this};
   // flag to use a pre-calculated neighbor map
