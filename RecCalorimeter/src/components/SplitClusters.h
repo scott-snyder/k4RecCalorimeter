5fd17a8f01e64f7a7bb4d1fd1e003e6cee0417e2
sss <sss@karma>
2026-03-27 11:07:28 -0400

    Move ICalorimeterTool here from k4FWCore.

--- RecCalorimeter/src/components/SplitClusters.h
+++ RecCalorimeter/src/components/SplitClusters.h
@@ -6,7 +6,6 @@
 
 // Interfaces
 #include "k4Interface/ICaloReadNeighboursMap.h"
-#include "k4Interface/ICalorimeterTool.h"
 #include "RecCaloCommon/ICellPositionsTool.h"
 #include "k4Interface/INoiseConstTool.h"
 
