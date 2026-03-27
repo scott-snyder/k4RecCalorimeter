d37ec37d9d04a6f341fcb8b899a58000e49d3989
sss <sss@karma>
2026-03-27 10:16:29 -0400

    Move ICellPositions tool here from k4FWCore.

--- RecFCCeeCalorimeter/src/components/CreateCaloCellPositionsFCCee.h
+++ RecFCCeeCalorimeter/src/components/CreateCaloCellPositionsFCCee.h
@@ -3,7 +3,7 @@
 
 // k4FWCore
 #include "k4FWCore/DataHandle.h"
-#include "k4Interface/ICellPositionsTool.h"
+#include "RecCaloCommon/ICellPositionsTool.h"
 
 // Gaudi
 #include "Gaudi/Algorithm.h"
@@ -47,7 +47,7 @@ public:
 
 private:
   /// Handle for tool to get positions
-  ToolHandle<ICellPositionsTool> m_cellPositionsTool{};
+  ToolHandle<k4::recCalo::ICellPositionsTool> m_cellPositionsTool{};
   /// Input collection
   mutable k4FWCore::DataHandle<edm4hep::CalorimeterHitCollection> m_hits{"hits/hits", Gaudi::DataHandle::Reader, this};
   /// Output collection
