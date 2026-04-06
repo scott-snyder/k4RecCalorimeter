bea8729ee5023bb3e773fc0959d66ce4da0e6b8e
sss <sss@karma>
2026-04-05 23:28:18 -0400

    Rewrite use of deprecated MetaDataHandles in terms of put/getParameter.

--- RecCalorimeter/src/components/CaloTopoCluster.h
+++ RecCalorimeter/src/components/CaloTopoCluster.h
@@ -9,7 +9,6 @@
 
 // Key4HEP
 #include "k4FWCore/DataHandle.h"
-#include "k4FWCore/MetaDataHandle.h"
 
 // Interfaces
 #include "RecCaloCommon/ICaloReadNeighboursMap.h"
@@ -109,9 +108,6 @@ private:
   // Cluster cells in collection
   mutable k4FWCore::DataHandle<edm4hep::CalorimeterHitCollection> m_clusterCellsCollection{
       "calo/clusterCells", Gaudi::DataHandle::Writer, this};
-  /// Handle for the cluster shape metadata to write
-  k4FWCore::MetaDataHandle<std::vector<std::string>> m_shapeParametersHandle{
-      m_clusterCollection, edm4hep::labels::ShapeParameterNames, Gaudi::DataHandle::Writer};
   /// Pointer to the geometry service
   SmartIF<IGeoSvc> m_geoSvc;
   /// Handle for the input tool
