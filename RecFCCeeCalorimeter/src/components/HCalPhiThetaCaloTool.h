41dd32f33c94f0af657ab2e69eea7b012a2f224e
sss <sss@karma>
2026-05-09 16:46:35 -0400

    Add implementation of ICalorimeterTool for Allegro HCal.
    Add HCalPhiThetaCaloTool, implementing ICalorimeterTool for the
    Allegro HCal (with theta segmentation).

---
+++ RecFCCeeCalorimeter/src/components/HCalPhiThetaCaloTool.h
@@ -0,0 +1,49 @@
+// This file's extension implies that it's C, but it's really -*- C++ -*-.
+/**
+ * @file RecFCCeeCalorimeter/src/components/HCalPhiThetaCaloTool.h
+ * @author scott snyder <snyder@bnl.gov>
+ * @date Feb, 2026
+ * @brief Calorimeter tool for Allegro HCal.
+ */
+
+
+#ifndef RECFCCEECALORIMETER_HCALPHITHETACALOTOOL_H
+#define RECFCCEECALORIMETER_HCALPHITHETACALOTOOL_H
+
+#include "RecCaloCommon/CalorimeterToolBase.h"
+
+/** @class HCalPhiThetaCaloTool
+ *
+ *  Manage IDs for HCal.
+ */
+class HCalPhiThetaCaloTool : public CalorimeterToolBase
+{
+public:
+  using CalorimeterToolBase::CalorimeterToolBase;
+  virtual ~HCalPhiThetaCaloTool() = default;
+
+
+  /** Gaudi initialize method.
+   */
+  virtual StatusCode initialize() override final;
+
+  /** Return the subdetector ID.
+   */
+  virtual int id() const final override;
+
+  /** Return a new indexer object for this subdetector.
+   */
+  virtual std::unique_ptr<k4::recCalo::ICaloIndexer> indexer() const override final;
+
+
+protected:
+  /** Fill vector with all existing cells for this geometry.
+   */
+  virtual StatusCode collectCells(std::vector<uint64_t>& cells) const override final;
+
+private:
+  /// Detector ID.
+  int m_id = -1;
+};
+
+#endif /* RECFCCEECALORIMETER_HCALPHITHETACALOTOOL_H */
