9e9fb83fea375958de18b3cfb3de6b1ef9fc79cf
sss <sss@karma>
2026-05-09 16:46:35 -0400

    Add implementation of ICalorimeterTool for Allegro HCal.
    Add HCalPhiThetaCaloTool, implementing ICalorimeterTool for the
    Allegro HCal (with theta segmentation).

---
+++ RecFCCeeCalorimeter/src/components/HCalPhiThetaCaloTool.cpp
@@ -0,0 +1,119 @@
+/**
+ * @file RecFCCeeCalorimeter/src/components/HCalPhiThetaCaloTool.cpp
+ * @author scott snyder <snyder@bnl.gov>
+ * @date Feb, 2026
+ * @brief Calorimeter tool for Allegro HCal.
+ */
+
+#include "HCalPhiThetaCaloTool.h"
+#include "RecCaloCommon/IDMapIndexer.h"
+#include "detectorSegmentations/FCCSWHCalPhiTheta_k4geo.h"
+#include "DD4hep/Detector.h"
+#include "k4FWCore/GaudiChecks.h"
+
+
+DECLARE_COMPONENT(HCalPhiThetaCaloTool)
+
+
+/** Gaudi initialize method.
+ */
+StatusCode HCalPhiThetaCaloTool::initialize()
+{
+  // Look up the detector ID.
+  std::string detidname = "DetID_";
+  if (readoutName() == "HCalBarrelReadout")
+    detidname += "HCAL_Barrel";
+  else
+    detidname += "HCAL_Endcap";
+
+  K4_GAUDI_CHECK( geoSvc().retrieve() );
+  m_id = geoSvc()->getDetector()->constant<int>(detidname);
+  if (m_id < 0) {
+    error() << "Can't find detector ID " << detidname << endmsg;
+    return StatusCode::FAILURE;
+  }
+
+  K4_GAUDI_CHECK( CalorimeterToolBase::initialize() );
+  return StatusCode::SUCCESS;
+}
+
+/** Return the subdetector ID.
+ */
+int HCalPhiThetaCaloTool::id() const
+{
+  return m_id;
+}
+
+/** Fill vector with all existing cells for this geometry.
+ */
+StatusCode HCalPhiThetaCaloTool::collectCells(std::vector<uint64_t>& cells) const
+{
+  cells.reserve (readoutName() == "HCalBarrelReadout" ? 210944 : 80896);
+  const auto* seg =
+    dynamic_cast<const dd4hep::DDSegmentation::FCCSWHCalPhiTheta_k4geo*> (readout().segmentation().segmentation());
+  if (!seg) {
+    error() << "Unable to cast segmentation pointer!!!! Tool only applicable to FCCSWHCalPhiTheta_k4geo "
+               "segmentation."
+            << endmsg;
+    return StatusCode::FAILURE;
+  }
+
+  const dd4hep::DDSegmentation::BitFieldCoder& decoder =
+    *readout().idSpec().decoder();
+
+  dd4hep::DDSegmentation::CellID cID = 0;
+  decoder.set(cID, "system", m_id);
+
+  size_t layer_id = decoder.index (seg->fieldNameLayer());
+  size_t theta_id = decoder.index (seg->fieldNameTheta());
+  size_t phi_id = decoder.index (seg->fieldNamePhi());
+
+  int numLayers = 0;
+  for (int n : seg->numLayers()) numLayers += n;
+  for (int layer = 0; layer < numLayers; ++layer) {
+    const std::vector<int>& thetaBins = seg->thetaBins (layer);
+    decoder.set(cID, layer_id, layer);
+    for (int theta : thetaBins) {
+      decoder.set(cID, theta_id, theta);
+      for (int phi = 0; phi < seg->phiBins(); ++phi) {
+        decoder.set(cID, phi_id, phi);
+        cells.push_back(cID);
+      }
+    }
+  }
+
+  return StatusCode::SUCCESS;
+}
+
+
+/** Return a new indexer object for this subdetector.
+ */
+std::unique_ptr<k4::recCalo::ICaloIndexer>
+HCalPhiThetaCaloTool::indexer() const
+{
+  const auto* seg =
+    dynamic_cast<const dd4hep::DDSegmentation::FCCSWHCalPhiTheta_k4geo*> (readout().segmentation().segmentation());
+  if (!seg) {
+    error() << "Unable to cast segmentation pointer!!!! Tool only applicable to FCCSWHCalPhiTheta_k4geo "
+               "segmentation."
+            << endmsg;
+    return nullptr;
+  }
+
+  using Indexer_t = k4::recCalo::IDMapIndexer<3>;
+  dd4hep::IDDescriptor idSpec = readout().idSpec();
+  std::vector<Indexer_t::FieldDesc_t> fields
+    { Indexer_t::IDMap_t::makeDesc (*idSpec.field(seg->fieldNameLayer())),
+      Indexer_t::IDMap_t::makeDesc (*idSpec.field(seg->fieldNameTheta())),
+      Indexer_t::IDMap_t::makeDesc (*idSpec.field(seg->fieldNamePhi())) };
+
+  const dd4hep::BitFieldElement& sysField = *idSpec.field("system");
+  if (sysField.offset() != 0) {
+    throw std::runtime_error ("Bad system field offset; must be zero");
+  }
+
+  return std::make_unique<Indexer_t> (this->id(),
+                                      sysField.width(),
+                                      fields, cellIDs(),
+                                      readoutName() == "HCalBarrelReadout" ? 900000 : 700000);
+}
