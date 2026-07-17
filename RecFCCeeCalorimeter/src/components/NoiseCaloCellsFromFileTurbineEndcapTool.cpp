57ecdb429ad236814571a33d44e2544aad3107fb
scott snyder <snyder@bnl.gov>
2026-07-17 12:54:51 -0400

    noiseIndex2

--- RecFCCeeCalorimeter/src/components/NoiseCaloCellsFromFileTurbineEndcapTool.cpp
+++ RecFCCeeCalorimeter/src/components/NoiseCaloCellsFromFileTurbineEndcapTool.cpp
@@ -2,21 +2,44 @@
 
 DECLARE_COMPONENT(NoiseCaloCellsFromFileTurbineEndcapTool)
 
-unsigned NoiseCaloCellsFromFileTurbineEndcapTool::getBin(CellID aCellId) const {
-  unsigned iHist = m_decoder->get(aCellId, m_index_activeField);
-  unsigned iRho = m_decoder->get(aCellId, "rho") + 1;
-  unsigned iZ = m_decoder->get(aCellId, "z") + 1;
 
-  unsigned NbinsZ = m_histoElecNoiseRMS.at(iHist)->GetNbinsX();
-  unsigned NbinsRho = m_histoElecNoiseRMS.at(iHist)->GetNbinsY();
+unsigned NoiseCaloCellsFromFileTurbineEndcapTool::getBin (const char* what,
+                                                          const TH1& h,
+                                                          unsigned iRho,
+                                                          unsigned iZ) const
+{
+  unsigned NbinsZ = h.GetNbinsX();
+  unsigned NbinsRho = h.GetNbinsY();
 
   unsigned ibin = 0;
   if (iRho > NbinsRho || iZ > NbinsZ) {
-    error() << "bins outside range of the histograms! Bins: " << iRho << "," << iZ
+    error() << "bins outside range of the " << what << " histograms! Bins: " << iRho << "," << iZ
             << " , Nbins in histogram: " << NbinsRho << "," << NbinsZ << endmsg;
   } else {
-    ibin = m_histoElecNoiseRMS.at(iHist)->GetBin(iZ, iRho);
+    ibin = h.GetBin(iZ, iRho);
+  }
+  return ibin;
+}
+
+
+StatusCode NoiseCaloCellsFromFileTurbineEndcapTool::initBinning (NoiseData& data,
+                                                                 const k4::recCalo::ICaloIndexer& indexer) const
+{
+  data.m_bins.resize (indexer.cellIDs().size());
+
+  for (uint64_t id : indexer.cellIDs()) {
+    unsigned ndx = indexer.index (id);
+    unsigned iHist = m_decoder->get(id, m_index_activeField);
+    unsigned iRho = m_decoder->get(id, "rho") + 1;
+    unsigned iZ = m_decoder->get(id, "z") + 1;
+
+    unsigned ibinRMS = getBin ("RMS", *data.m_histoElecNoiseRMS.at(iHist), iRho, iZ);
+    unsigned ibinOffset = 0;
+    if (m_setNoiseOffset)
+      ibinOffset = getBin ("Offset", *data.m_histoElecNoiseOffset.at(iHist), iRho, iZ);
+
+    data.m_bins.at(ndx) = std::make_pair(ibinRMS, ibinOffset);
   }
 
-  return ibin;
+  return StatusCode::SUCCESS;
 }
