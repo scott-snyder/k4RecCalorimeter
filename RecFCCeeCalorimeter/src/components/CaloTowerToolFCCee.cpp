eef20f233ef4069f248dafb9d1c86a9872065176
scott snyder <snyder@bnl.gov>
2026-07-24 13:36:56 -0400

    Refresh of towersort

--- RecFCCeeCalorimeter/src/components/CaloTowerToolFCCee.cpp
+++ RecFCCeeCalorimeter/src/components/CaloTowerToolFCCee.cpp
@@ -129,7 +129,7 @@ uint CaloTowerToolFCCee::CellsIntoTowers(std::vector<std::vector<float>>& aTower
   int iPhi = 0;
   uint clusteredCells = 0;
 
-  std::vector<float> negTowers (m_nThetaTower * m_nPhiTower);
+  std::vector<double> dTowers (m_nThetaTower * m_nPhiTower);
 
   for (const auto& cell : *aCells) {
     float cellX = cell.getPosition().x;
@@ -153,12 +153,7 @@ uint CaloTowerToolFCCee::CellsIntoTowers(std::vector<std::vector<float>>& aTower
     // debug() << "Cell: iTheta = " << iTheta << " iPhi = " << iPhi << " iPhi(cyclic) = " << phiIndexTower(iPhi) <<
     // endmsg;
     double et = cell.getEnergy() * sin(cellTheta);
-    if (et > 0) {
-      aTowers[iTheta][phiIndexTower(iPhi)] += et;
-    }
-    else {
-      negTowers[iTheta*m_nPhiTower + phiIndexTower(iPhi)] += et;
-    }
+    dTowers[iTheta*m_nPhiTower + phiIndexTower(iPhi)] += et;
     if (fillTowersCells) {
       clusteredCells++;
       m_cellsInTowers[std::make_pair(iTheta, phiIndexTower(iPhi))].push_back(cell);
@@ -172,7 +167,7 @@ uint CaloTowerToolFCCee::CellsIntoTowers(std::vector<std::vector<float>>& aTower
 
   for (size_t jTheta = 0; (int)jTheta < m_nThetaTower; ++jTheta) {
     for (size_t jPhi = 0; (int)jPhi < m_nPhiTower; ++jPhi) {
-      aTowers[jTheta][jPhi] += negTowers[jTheta*m_nPhiTower + jPhi];
+      aTowers[jTheta][jPhi] = dTowers[jTheta*m_nPhiTower + jPhi];
     }
   }
   for (auto& p : m_cellsInTowers) {
